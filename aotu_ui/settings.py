0632d870a0b0b09da8ca8a219702fe84d98d9a80
wenxianping <931128603@qq.com>
2018-05-03 16:39:22 +0800

    1、兼容图片显示 2、修改报表字段取错

--- aotu_ui/settings.py
+++ aotu_ui/settings.py
@@ -30,7 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -75,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'aotu_ui.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -89,7 +87,6 @@ DATABASES = {
         'PORT': y["db"]["port"],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -109,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -123,8 +119,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = '/usr/local/py_work/appium_warp/log'
+MEDIA_URL = '/image/'
--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
+from django.views.static import serve
+
+from aotu_ui.settings import MEDIA_ROOT
 from home import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Matches any html file - to be used for gentella
@@ -9,5 +14,7 @@ urlpatterns = [
 
     # The home page
     url(r'^$', views.index, name='index'),
+    url(r'^image/(?P<path>.*)/$', serve, {"document_root": MEDIA_ROOT}),
 ]
+
 

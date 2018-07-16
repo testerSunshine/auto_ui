4893dc0eab314e0437a141038df37bba413e4c3f
wenxianping <931128603@qq.com>
2018-07-16 16:38:52 +0800

    1、兼容脚本

--- aotu_ui/settings.py
+++ aotu_ui/settings.py
@@ -140,5 +140,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = '/usr/local/py_work/appium_warp/log'
+MEDIA_ROOT = '/usr/local/log'
 MEDIA_URL = '/image/'
